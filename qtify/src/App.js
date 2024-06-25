89eff7d1c1f5e72e0c9eee1e2933b3f10673db3c
dineshkumardk26 <mysystem978@gmail.com>
2024-06-25 21:26:20 +0530

    NavBar1

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -23,12 +23,8 @@
 // }
 import { useEffect, useState } from "react";
 import "./App.css";
-import Card from "./components/Card/Card";
-import HeroSection from "./components/HeroSection/HeroSection";
 //@ts-ignore
 import NavBar from "./components/NavBar/NavBar";
-import { fetchTopAlbums } from "./components/api/api";
-import Section from "./components/Sections/Section";
 
 function App() {
   const [topAlbumData, settopAlbumData] = useState([]);
