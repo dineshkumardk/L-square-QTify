9c7d0d287023be4ccb04bce5a42aa383c2d763db
dineshkumardk26 <mysystem978@gmail.com>
2024-06-26 20:52:58 +0530

    Fix path issues for Album

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import AlbumSongsPage from "./Components/Album/AlbumSongsPage";
+import AlbumSongsPage from "./Components/Album/Album";
 import HomePage from './Components/Home Page/HomePage';
 import { Route, Routes } from "react-router-dom";
 
